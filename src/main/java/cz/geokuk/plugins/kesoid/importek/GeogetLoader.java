5bcb5170bec5217b9c35e587ab0bca6682a0f0d9
Daniel Stahr <dan.stahr@gmail.com>
2016-02-21 18:26:15 +0000

    Improved GeoGet data loader

--- src/main/java/cz/geokuk/plugins/kesoid/importek/GeogetLoader.java
+++ src/main/java/cz/geokuk/plugins/kesoid/importek/GeogetLoader.java
@@ -1,16 +1,21 @@
 package cz.geokuk.plugins.kesoid.importek;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Closeables;
 import com.google.common.io.Files;
 import cz.geokuk.core.coordinates.Wgs;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.concurrent.Future;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.zip.*;
 
 /**
  * Loads data from a GeoGet database.
@@ -18,9 +23,33 @@ import java.util.zip.ZipFile;
  * <p>Information about GeoGet DB schema: http://geoget.ararat.cz/doku.php/user:databaze
  */
 public class GeogetLoader extends Nacitac0 {
-  private static final String GEOGET_DB_QUERY =
-      "SELECT id, x as lat, y as lon, name, author, cachetype, cachesize, difficulty, terrain, cachestatus " +
-          "FROM geocache";
+  private static final String GEOGET_CACHES_QUERY =
+      Joiner.on('\n').join(
+          "SELECT",
+          "  geocache.id as id,",
+          "  geocache.x as lat,",
+          "  geocache.y as lon,",
+          "  geocache.name as name,",
+          "  geocache.author as author,",
+          "  geocache.cachetype as cachetype,",
+          "  geocache.cachesize as cachesize,",
+          "  geocache.difficulty as difficulty,",
+          "  geocache.terrain as terrain,",
+          "  geocache.cachestatus as cachestatus,",
+          "  geocache.gs_ownerid as gs_ownerid,",
+          "  geocache.dthidden as dthidden,",
+          "  geocache.country as country,",
+          "  geocache.state as state,",
+          "  geolist.shortdesc as shortdesc,",
+          "  geolist.hint as hint",
+          "FROM geocache",
+          "LEFT JOIN geolist",
+          "  ON geocache.id = geolist.id");
+
+  private static final String GEOGET_WAYPOINTS_QUERY = "SELECT id, x as lat, y as lon, prefixid, wpttype, name" +
+      " FROM waypoint";
+
+  private static final String DATE_FORMAT_TEMPLATE = "%d-%02d-%02dT00:00:00.000";
 
   private static final ImmutableSet<String> SUPPORTED_FILE_EXTENSIONS = ImmutableSet.of("db3");
 
@@ -35,8 +64,18 @@ public class GeogetLoader extends Nacitac0 {
       throw new IllegalArgumentException("Cannot load from file " + file);
     }
     try (Connection c = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
-         Statement statement = c.createStatement();
-         ResultSet rs = statement.executeQuery(GEOGET_DB_QUERY)) {
+         Statement statement = c.createStatement()) {
+        loadCaches(statement, builder, future);
+        loadWaypoints(statement, builder, future);
+      }
+    catch (SQLException e) {
+      throw new IOException("Unable to load from " + file, e);
+    }
+  }
+
+  private void loadCaches(Statement statement, IImportBuilder builder, Future<?> future)
+      throws SQLException, IOException {
+    try (ResultSet rs = statement.executeQuery(GEOGET_CACHES_QUERY)) {
       while (rs.next()) {
         if (future != null && future.isCancelled()) {
           return;
@@ -44,7 +83,7 @@ public class GeogetLoader extends Nacitac0 {
         GpxWpt gpxWpt = new GpxWpt();
         gpxWpt.wgs = new Wgs(rs.getDouble("lat"), rs.getDouble("lon"));
         gpxWpt.name = rs.getString("id");
-        if (gpxWpt.name.length() > 1) {
+        if (gpxWpt.name != null && gpxWpt.name.length() > 1) {
           String prefix = gpxWpt.name.substring(0, 2);
           String sym = ID_PREFIX_TO_SYM.get(prefix);
           if (sym != null) {
@@ -52,13 +91,25 @@ public class GeogetLoader extends Nacitac0 {
           }
         }
 
+        gpxWpt.time = formatDateTime(rs.getInt("dthidden"));
+
         Groundspeak groundspeak = new Groundspeak();
+        groundspeak.ownerid = rs.getInt("gs_ownerid");
         groundspeak.name = rs.getString("name");
         groundspeak.placedBy = rs.getString("author");
+        groundspeak.owner = groundspeak.placedBy;
         groundspeak.type = rs.getString("cachetype");
         groundspeak.container = rs.getString("cachesize");
         groundspeak.difficulty = rs.getString("difficulty");
         groundspeak.terrain = rs.getString("terrain");
+        groundspeak.country = rs.getString("country");
+        groundspeak.state = rs.getString("state");
+
+        InputStream shortDescBlob = rs.getBinaryStream("shortdesc");
+        InflaterInputStream inflaterStream = new InflaterInputStream(shortDescBlob);
+        groundspeak.shortDescription = CharStreams.toString(new InputStreamReader(inflaterStream, Charsets.UTF_8));
+        Closeables.closeQuietly(inflaterStream);
+        groundspeak.encodedHints = rs.getString("hint");
 
         int cacheStatus = rs.getInt("cachestatus");
         switch (cacheStatus) {
@@ -82,9 +133,34 @@ public class GeogetLoader extends Nacitac0 {
             gpxWpt.groundspeak.difficulty, gpxWpt.groundspeak.terrain);
         builder.addGpxWpt(gpxWpt);
       }
-    } catch (SQLException e) {
-      throw new IOException("Unable to load from " + file, e);
     }
+  }
+
+  private void loadWaypoints(Statement statement, IImportBuilder builder, Future<?> future) throws SQLException {
+    try (ResultSet rs = statement.executeQuery(GEOGET_WAYPOINTS_QUERY)) {
+      while (rs.next()) {
+        if (future != null && future.isCancelled()) {
+          return;
+        }
+        GpxWpt gpxWpt = new GpxWpt();
+        gpxWpt.wgs = new Wgs(rs.getDouble("lat"), rs.getDouble("lon"));
+        String parentId = rs.getString("id");
+        if (parentId != null && parentId.length() > 1) {
+          String suffix = parentId.substring(2);
+          gpxWpt.name = rs.getString("prefixid") + suffix;
+        }
+        gpxWpt.sym = rs.getString("wpttype");
+        gpxWpt.desc = rs.getString("name");
+        builder.addGpxWpt(gpxWpt);
+      }
+    }
+  }
+
+  private String formatDateTime(int yyyymmddDate) {
+    int day = yyyymmddDate % 100;
+    int month = (yyyymmddDate / 100) % 100;
+    int year = (yyyymmddDate / 10000);
+    return String.format(DATE_FORMAT_TEMPLATE, year, month, day);
   }
 
   @Override
