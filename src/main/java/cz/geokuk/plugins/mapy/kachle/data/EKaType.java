636578565b85092a0b0eb02801b035e7a73f7efa
Bohuslav Roztočil <bohuslav.roztocil@i.cz>
2023-05-11 17:42:01 +0200

    geokuk-177 Konfigurovatelné mapy: Generalizace EKaType II, úklid

--- src/main/java/cz/geokuk/plugins/mapy/kachle/data/EKaType.java
+++ src/main/java/cz/geokuk/plugins/mapy/kachle/data/EKaType.java
@@ -1,23 +1,22 @@
 package cz.geokuk.plugins.mapy.kachle.data;
 
+import javax.swing.*;
 import java.awt.event.KeyEvent;
-
-import javax.swing.KeyStroke;
 
 public enum EKaType implements KaType {
 
     // Mapové podklady, dále neprůhledné
-	BASE_M(false, 0, 19, 19, "Základní", "Základní mapa se silnicemi.", KeyEvent.VK_Z, KeyStroke.getKeyStroke('z'), new MapyCzUrlBuilder("base-m")),
-	TURIST_M(false, 0, 19, 19, "Turistická", "Turistická mapa.", KeyEvent.VK_T, KeyStroke.getKeyStroke('t'), new MapyCzUrlBuilder("turist-m")),
-	OPHOTO_M(true, 0, 20, 20, "Letecká", "Letecká ortho foto mapa", KeyEvent.VK_L, KeyStroke.getKeyStroke('f'), new MapyCzUrlBuilder("ophoto-m")),
-	WTURIST_WINTER_M(false, 0, 19, 19, "Turistická zimní", "Zimní turistická mapa.", KeyEvent.VK_M, KeyStroke.getKeyStroke('w'), new MapyCzUrlBuilder("wturist_winter-m")),
-	OPHOTO1415_M(true, 0, 20, 20, "Letecká 2015", "Starší fotomapa", 0, null, new MapyCzUrlBuilder("ophoto1415-m")),
-	OPHOTO1012_M(true, 0, 19, 19, "Letecká 2012", "Starší fotomapa", 0, null, new MapyCzUrlBuilder("ophoto1012-m")),
-	OPHOTO0406_M(true, 0, 19, 19, "Letecká 2006", "Starší fotomapa", KeyEvent.VK_6, null, new MapyCzUrlBuilder("ophoto0406-m")),
-	OPHOTO0203_M(true, 0, 18, 18, "Letecká 2003", "Starší fotomapa", KeyEvent.VK_3, null, new MapyCzUrlBuilder("ophoto0203-m")),
-	ZEMEPIS_M(false, 0, 18, 18, "Zeměpisná", "Zeměpisná mapa", KeyEvent.VK_G, KeyStroke.getKeyStroke('g'), new MapyCzUrlBuilder("zemepis-m")),
-	BASE_M_TRAF_DOWN(false, 0, 19, 19, "Dopravní", "Dopravní mapa taková vyšedlá.", 0, null, new MapyCzUrlBuilder("base-m-traf-down")),
-	ARMY2_M(true, 0, 15, 15, "Historická", "Historická mapa z let 1836-52", KeyEvent.VK_H, KeyStroke.getKeyStroke('h'), new MapyCzUrlBuilder("army2-m")),
+    BASE_M(false, 0, 19, 19, "Základní", "Základní mapa se silnicemi.", KeyEvent.VK_Z, KeyStroke.getKeyStroke('z'), new MapyCzUrlBuilder("base-m")),
+    TURIST_M(false, 0, 19, 19, "Turistická", "Turistická mapa.", KeyEvent.VK_T, KeyStroke.getKeyStroke('t'), new MapyCzUrlBuilder("turist-m")),
+    OPHOTO_M(true, 0, 20, 20, "Letecká", "Letecká ortho foto mapa", KeyEvent.VK_L, KeyStroke.getKeyStroke('f'), new MapyCzUrlBuilder("ophoto-m")),
+    WTURIST_WINTER_M(false, 0, 19, 19, "Turistická zimní", "Zimní turistická mapa.", KeyEvent.VK_M, KeyStroke.getKeyStroke('w'), new MapyCzUrlBuilder("wturist_winter-m")),
+    OPHOTO1415_M(true, 0, 20, 20, "Letecká 2015", "Starší fotomapa", 0, null, new MapyCzUrlBuilder("ophoto1415-m")),
+    OPHOTO1012_M(true, 0, 19, 19, "Letecká 2012", "Starší fotomapa", 0, null, new MapyCzUrlBuilder("ophoto1012-m")),
+    OPHOTO0406_M(true, 0, 19, 19, "Letecká 2006", "Starší fotomapa", KeyEvent.VK_6, null, new MapyCzUrlBuilder("ophoto0406-m")),
+    OPHOTO0203_M(true, 0, 18, 18, "Letecká 2003", "Starší fotomapa", KeyEvent.VK_3, null, new MapyCzUrlBuilder("ophoto0203-m")),
+    ZEMEPIS_M(false, 0, 18, 18, "Zeměpisná", "Zeměpisná mapa", KeyEvent.VK_G, KeyStroke.getKeyStroke('g'), new MapyCzUrlBuilder("zemepis-m")),
+    BASE_M_TRAF_DOWN(false, 0, 19, 19, "Dopravní", "Dopravní mapa taková vyšedlá.", 0, null, new MapyCzUrlBuilder("base-m-traf-down")),
+    ARMY2_M(true, 0, 15, 15, "Historická", "Historická mapa z let 1836-52", KeyEvent.VK_H, KeyStroke.getKeyStroke('h'), new MapyCzUrlBuilder("army2-m")),
 
 // Nefunkční mapy k 16.11.2019
 //	OPEN_STREAT(false, 0, 18, 18, "Openstreetmap", "Openstreetmap.", KeyEvent.VK_O, KeyStroke.getKeyStroke('o'), new OpenStreatMapUrlBuilder("https://b.tile.openstreetmap.org/")),
@@ -29,101 +28,101 @@ public enum EKaType implements KaType {
 //	TUR_FREEMAP_SK_K(false, 0, 18, 18, "Slovensko lyžařská  ", "turistika.freemap.sk - lyžařská mapa", 0, null, new OpenStreatMapUrlBuilder("http://c.freemap.sk/K/")),
 //
 
-	// Nefunguje, jakási ochrana přes kukačku
-	// HIKING_SK_TOPO (true, false, 0, 18, 18, "Slovensko turistická ", "mapy.hiking.sk - topo", 0, null, new OpenStreatMapUrlBuilder("http://mapy.hiking.sk/layers/topo/")),
+    // Nefunguje, jakási ochrana přes kukačku
+    // HIKING_SK_TOPO (true, false, 0, 18, 18, "Slovensko turistická ", "mapy.hiking.sk - topo", 0, null, new OpenStreatMapUrlBuilder("http://mapy.hiking.sk/layers/topo/")),
 
-	// Todo vyřešit problémy s certifikátem
-	// OPEN_CYKLO(true, false, 0, 18, 18, "Open cyclo", "Open streat map.", KeyEvent.VK_Y, KeyStroke.getKeyStroke('c') , new OpenStreatMapUrlBuilder("https://b.tile.thunderforest.com/cycle/")),
+    // Todo vyřešit problémy s certifikátem
+    // OPEN_CYKLO(true, false, 0, 18, 18, "Open cyclo", "Open streat map.", KeyEvent.VK_Y, KeyStroke.getKeyStroke('c') , new OpenStreatMapUrlBuilder("https://b.tile.thunderforest.com/cycle/")),
 
-	;
+    ;
 
-	// super("Turistické trasy");
-	// putValue(SHORT_DESCRIPTION, "Turistické trasy, červená, modrá, zelená, žlutá.");
-	// putValue(MNEMONIC_KEY, KeyEvent.VK_U);
-	// putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke('u'));
+    // super("Turistické trasy");
+    // putValue(SHORT_DESCRIPTION, "Turistické trasy, červená, modrá, zelená, žlutá.");
+    // putValue(MNEMONIC_KEY, KeyEvent.VK_U);
+    // putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke('u'));
 
-	private final int minMoumer;
-	private final int maxMoumer;
-	private final int maxAutoMoumer;
-	private final String nazev;
-	private final String popis;
-	private final int klavesa;
-	private final KeyStroke keyStroke;
-	private KachleUrlBuilder urlBuilder;
+    private final int minMoumer;
+    private final int maxMoumer;
+    private final int maxAutoMoumer;
+    private final String nazev;
+    private final String popis;
+    private final int klavesa;
+    private final KeyStroke keyStroke;
+    private final KachleUrlBuilder urlBuilder;
 
-	/**
-	 * @param podklad
-	 *            true, pokud se jedná o neprůhledný podlad, false jinak.
-	 * @param jeMozneNavrsitTexty
-	 *            zde je možné ještě aplikovat samostatnou vrstvu s texty. Zadejte true, pokud mapa texty neobsahuje (napříkald fotomapy).
-	 * @param minMoumer
-	 *            Minimáklní měřítko, ke kterému jsou podklady. Věřme, že to bude dne světšinou 0. (U starých seznamových map to bylo 3 nebo 4).
-	 * @param maxMoumer
-	 *            Maximální měřítko, pro který jsou kachle.
-	 * @param maxAutoMoumer
-	 *            už přesně nevím, nutno analyzovat, nastavujem to stejnějako maxMoumer
-	 * @param nazev
-	 *            Název mapy, tak se objeví v menu.
-	 * @param popis
-	 *            Bližší popis mapy, objeví se jako tooltip v menu.
-	 * @param klavesa
-	 *            Hot-key, která mapu vyvolá.
-	 * @param keyStroke
-	 *            Písmeno z nazev, které lze použít pro výběr při rozbaleném menu.
-	 * @param urlBuilder
-	 *            Implementace třídy, která sestaví URL pro zobrazení mapy.
-	 */
-	private EKaType(final boolean jeMozneNavrsitTexty, final int minMoumer, final int maxMoumer, final int maxAutoMoumer, final String nazev, final String popis, final int klavesa,
-	        final KeyStroke keyStroke, final KachleUrlBuilder urlBuilder) {
-		this.minMoumer = minMoumer;
-		this.maxMoumer = maxMoumer;
-		this.maxAutoMoumer = maxAutoMoumer;
-		this.nazev = nazev;
-		this.popis = popis;
-		this.klavesa = klavesa;
-		this.keyStroke = keyStroke;
-		this.urlBuilder = urlBuilder;
+    /**
+     * \@param podklad                   true, pokud se jedná o neprůhledný podlad, false jinak.
+     *
+     * @param ignoredJeMozneNavrsitTexty zde je možné ještě aplikovat samostatnou vrstvu s texty. Zadejte true, pokud mapa texty neobsahuje (napříkald fotomapy).
+     * @param minMoumer                  Minimáklní měřítko, ke kterému jsou podklady. Věřme, že to bude dne světšinou 0. (U starých seznamových map to bylo 3 nebo 4).
+     * @param maxMoumer                  Maximální měřítko, pro který jsou kachle.
+     * @param maxAutoMoumer              už přesně nevím, nutno analyzovat, nastavujem to stejnějako maxMoumer
+     * @param nazev                      Název mapy, tak se objeví v menu.
+     * @param popis                      Bližší popis mapy, objeví se jako tooltip v menu.
+     * @param klavesa                    Hot-key, která mapu vyvolá.
+     * @param keyStroke                  Písmeno z nazev, které lze použít pro výběr při rozbaleném menu.
+     * @param urlBuilder                 Implementace třídy, která sestaví URL pro zobrazení mapy.
+     */
+    EKaType(
+            final boolean ignoredJeMozneNavrsitTexty,
+            final int minMoumer,
+            final int maxMoumer,
+            final int maxAutoMoumer,
+            final String nazev,
+            final String popis,
+            final int klavesa,
+            final KeyStroke keyStroke,
+            final KachleUrlBuilder urlBuilder
+    ) {
+        this.minMoumer = minMoumer;
+        this.maxMoumer = maxMoumer;
+        this.maxAutoMoumer = maxAutoMoumer;
+        this.nazev = nazev;
+        this.popis = popis;
+        this.klavesa = klavesa;
+        this.keyStroke = keyStroke;
+        this.urlBuilder = urlBuilder;
 
-	}
+    }
 
-	@Override
+    @Override
     public KeyStroke getKeyStroke() {
-		return keyStroke;
-	}
+        return keyStroke;
+    }
 
-	@Override
+    @Override
     public int getKlavesa() {
-		return klavesa;
-	}
+        return klavesa;
+    }
 
-	@Override
+    @Override
     public int getMaxAutoMoumer() {
-		return maxAutoMoumer;
-	}
+        return maxAutoMoumer;
+    }
 
-	@Override
+    @Override
     public int getMaxMoumer() {
-		return maxMoumer;
-	}
+        return maxMoumer;
+    }
 
-	@Override
+    @Override
     public int getMinMoumer() {
-		return minMoumer;
-	}
+        return minMoumer;
+    }
 
-	@Override
+    @Override
     public String getNazev() {
-		return nazev;
-	}
+        return nazev;
+    }
 
-	@Override
+    @Override
     public String getPopis() {
-		return popis;
-	}
+        return popis;
+    }
 
-	@Override
+    @Override
     public KachleUrlBuilder getUrlBuilder() {
-		return urlBuilder;
-	}
+        return urlBuilder;
+    }
 
 }
